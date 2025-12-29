73c147d3e62a969111bfc95f5ba152ddae8c8c0d
BayramYAVUZ <bayramyvz1232003@gmail.com>
2025-12-29 20:18:19 +0300

    Frontend versiyon guncellemesi v2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -28,7 +28,7 @@ export default function App() {
         </header>
 
         <div className="container">
-          <h1>Flight Search</h1>
+          <h1>Flight Search V</h1>
 
           <div className="filters">
             <input
